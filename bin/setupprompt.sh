e550bcb0f7f4b16111ba9f08007320e8744d715c
Martin Pittermann <martinp2250@gmail.com>
2018-11-02 10:03:25 +0100

    updated bash PS1

--- bin/setupprompt.sh
+++ bin/setupprompt.sh
@@ -1,8 +1,32 @@
-RESET="\[\033[0m\]"
+RED="\[\033[1;31m\]"
 GREEN="\[\033[1;32m\]"
-RED="\[\033[31;1m\]"
+YELLOW="\[\033[1;33m\]"
 BLUE="\[\033[1;34m\]"
+MAGENTA="\[\033[0;35m\]"
+CYAN="\[\033[0;36m\]"
+RESET="\[\033[0m\]"
 
-SELECT="if [ \$? != 0 ]; then echo \"${RED}\"; fi"
+function PS_Exit
+{
+	ES=$?
 
-PS1="${GREEN}\u@\h ${BLUE}\W \`${SELECT}\`\\$ ${RESET}"
+	if [ $ES -ne 0 ]; then
+		echo -en "\[\\033[1;31m\]$ES"
+	else
+		echo -en "\[\\033[1;32m\]✔"
+	fi
+}
+
+function timer_start {
+	timer=${timer:-$SECONDS}
+}
+
+trap 'timer_start' DEBUG
+
+function PS_Set_Prompt
+{
+	PS1="$(PS_Exit) ${GREEN}$(($SECONDS - $timer))s ${YELLOW}\$(date +%H:%M) ${GREEN}\u@\h ${BLUE}\W\n${GREEN}\$${RESET}"
+	unset timer
+}
+
+PROMPT_COMMAND=PS_Set_Prompt
