e8f11ce6fb2213743e7e1e9ac4cace899966c53b
Martin Pittermann <martinp2250@gmail.com>
2018-11-25 15:52:51 +0100

    added missing quotes

--- configuration/bash/common/commands.sh
+++ configuration/bash/common/commands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # upload file to transfer.sh
-function transfer { curl --upload-file $1 https://transfer.sh/$1; echo; }
+function transfer { curl --upload-file "$1" "https://transfer.sh/$1"; echo; }
 
 # start program and detatch from shell
 function run { $@ > /dev/null 2>&1 & disown > /dev/null 2>&1; }
@@ -10,26 +10,26 @@ function run { $@ > /dev/null 2>&1 & disown > /dev/null 2>&1; }
 function killjobs { kill $(jobs -p); }
 
 # open new terminal window with optional directory
-function term { gnome-terminal --quiet --working-directory=${1:-$(pwd)}; }
+function term { gnome-terminal --quiet --working-directory="${1:-$(pwd)}"; }
 
 # make new directory (can contain new subdirectories) and cd into it
-function mkcd { mkdir $1 && cd $1; }
+function mkcd { mkdir "$1" && cd "$1"; }
 
 # extract compressed file
 function extract () {
-	if [ -f $1 ] ; then
-		case $1 in
-			*.tar.bz2)   tar xvjf $1    ;;
-			*.tar.gz)    tar xvzf $1    ;;
-			*.bz2)       bunzip2 $1     ;;
-			*.rar)       unrar x $1     ;;
-			*.gz)        gunzip $1      ;;
-			*.tar)       tar xvf $1     ;;
-			*.tbz2)      tar xvjf $1    ;;
-			*.tgz)       tar xvzf $1    ;;
-			*.zip)       unzip $1       ;;
-			*.Z)         uncompress $1  ;;
-			*.7z)        7z x $1        ;;
+	if [ -f "$1" ] ; then
+		case "$1" in
+			*.tar.bz2)   tar xvjf "$1"    ;;
+			*.tar.gz)    tar xvzf "$1"    ;;
+			*.bz2)       bunzip2 "$1"     ;;
+			*.rar)       unrar x "$1"     ;;
+			*.gz)        gunzip "$1"      ;;
+			*.tar)       tar xvf "$1"     ;;
+			*.tbz2)      tar xvjf "$1"    ;;
+			*.tgz)       tar xvzf "$1"    ;;
+			*.zip)       unzip "$1"       ;;
+			*.Z)         uncompress "$1"  ;;
+			*.7z)        7z x "$1"        ;;
 			*)           echo "don't know how to extract '$1'..." ;;
 		esac
 	else
@@ -47,5 +47,5 @@ up(){
 	if [ -z "$d" ]; then
 		d=..
 	fi
-	cd $d
+	cd "$d"
 }
