712968a866563e1a9d2cefb0f71fe00920d0a23c
Martin Pittermann <martinp2250@gmail.com>
2019-01-08 09:18:53 +0100

    fix bash setup

--- configuration/bash/setup.sh
+++ configuration/bash/setup.sh
@@ -5,11 +5,12 @@
 
 BASHPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-ln -s ~/linux-common/bin ~/
-rm -f "/home/$USER/.bashrc"
-rm -f "/home/$USER/.bash_profile"
-ln -s "$BASHPATH/bashrc.sh" "/home/$USER/.bashrc"
-ln -s "$BASHPATH/bashrc.sh" "/home/$USER/.bash_profile"
-mkdir -p "/home/$USER/.bash"
-rm -f "/home/$USER/.bash/common"
-ln -s "$BASHPATH/common" "/home/$USER/.bash/"
+rm -f $HOME/bin
+ln -s $BASEPATH/../../bin $HOME/bin
+rm -f "$HOME/.bashrc"
+rm -f "$HOME/.bash_profile"
+ln -s "$BASHPATH/bashrc.sh" "$HOME/.bashrc"
+ln -s "$BASHPATH/bashrc.sh" "$HOME/.bash_profile"
+mkdir -p "$HOME/.bash"
+rm -f "$HOME/.bash/common"
+ln -s "$BASHPATH/common" "$HOME/.bash/"
