ae5185565d08a8d658b4540020df3331803d54f5
Martin Pittermann <martinp2250@gmail.com>
2019-01-23 17:17:54 +0100

    fix update-linux-common

--- configuration/bash/common/update-linux-common.sh
+++ configuration/bash/common/update-linux-common.sh
@@ -3,15 +3,16 @@
 STATUSFILE="/run/user/$UID/update-linux-common"
 
 if [ -f $STATUSFILE ]; then
-	printf "${RED}"
-	printf "###############################\n"
-	printf "# linux-common not up to date #\n"
-	printf "###############################\n"
-	printf "${RESET}"
+	echo "${RED}"
+	echo "###############################"
+	echo "# linux-common not up to date #"
+	echo "###############################"
+	echo "${RESET}"
 	read -p "update linux-common? (y/n): " -r
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		cd $HOME/linux-common || (printf "${RED}linux-common not found${RESET}\n"; exit 1)
+		pushd $HOME/linux-common || (printf "${RED}linux-common not found${RESET}\n"; exit 1)
 		git pull && rm ${STATUSFILE}
+		popd
 	fi
 fi
